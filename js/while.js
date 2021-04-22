28a32808d93d166629af8b7ef1680454c491e428
Amado Azua III <amado.azua3@gmail.com>
2021-04-22 17:04:39 -0500

    Do-while loop exercise

---
+++ js/while.js
@@ -0,0 +1,57 @@
+"use strict";
+
+// var i = 0;
+// var num = 1;
+//
+// while(i < 16) {
+//
+//     var multiply = num * 2
+//     console.log(multiply);
+//     num = multiply;
+//
+//     i++;
+// }
+
+// // TODO: A parking lot with 30 parking spaces wants to display the amount of spaces available as it gets filled.
+//  Let's assume that there are allowing two cars into the parking lot at a time. As those cars take up spaces,
+//  write a do-while loop that prints the spaces available. If the parking lot gets filled, print "this parking lot
+//  is now full" to the console.
+// // HINT: think of a countdown, remember if statements can be used inside of your statements
+
+// var cars = 2;
+// var availableSpaces = 30;
+//
+// do {
+//     availableSpaces = availableSpaces - cars;
+//     console.log(`Available parking spaces are ${availableSpaces}`)
+//
+//     if(availableSpaces === 0){
+//         console.log("This parking lot is now full");
+//     }
+// }while (availableSpaces >= 1)
+
+
+//----------------------------------------------
+
+// This is how you get a random number between 50 and 100
+var allCones = Math.floor(Math.random() * 50) + 50;
+// This expression will generate a random number between 1 and 5
+var random = Math.floor(Math.random() * 5) + 1;
+
+do{
+
+    allCones = allCones - random;
+    console.log(random);
+    console.log("-----------------");
+    console.log(`We have ${allCones} left`);
+
+    if(allCones < random || allCones === 0){
+        console.log("Sorry we don't have enough");
+        break;
+    }
+
+}while(allCones >= 1)
+
+
+
+
