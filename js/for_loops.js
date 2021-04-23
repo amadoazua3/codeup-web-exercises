c1c33b2c4967703fd79d9a4b3a4170ae37f83163
Amado Azua III <amado.azua3@gmail.com>
2021-04-23 16:52:38 -0500

    Javascript loops exercises

--- js/for_loops.js
+++ js/for_loops.js
@@ -17,11 +17,9 @@
 
 //------------------------------------------------ Exercise 3
 
-
-
 // for(var i = 0; i < 10; i++){
 //
-//     var random = Math.floor(Math.random() * 20) + 180;
+//     var random = Math.floor(Math.random() * (200 - 20)) + 20;
 //
 //     if(random % 2 === 0){
 //         console.log(random + " is even");
@@ -32,6 +30,21 @@
 
 //------------------------------------------------- Exercise 4
 
+//Grady's Solution
+// let numberPyramid = function(start, end){
+//     for(let i = start; i <= end; i++){
+//         var x = String(i);
+//         x = x.repeat(i);
+//         console.log(x);
+//     }
+// }
+//
+// numberPyramid(1, 9);
+
+// Laura's solution
+// for(let i = 1; i <= 9; i++){
+//     console.log(i.toString().repeat(i));
+// }
 
 //------------------------------------------------- Exercise 5
 
