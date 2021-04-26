7f7373377a18ba378c272ab5c4a0f6448ed0bea7
Amado Azua III <amado.azua3@gmail.com>
2021-04-26 14:23:46 -0500

    Javascript array exercises

---
+++ js/arrays-manipulation-lecture.js
@@ -0,0 +1,46 @@
+(function (){
+
+    "use strict";
+
+    //TODO:
+    // -> .length - SUPER IMPORTANT!
+    // -> .forEach() - ALSO IMPORTANT!
+    // -> FOR LOOP - STILL IMPORTANT!
+
+
+    // var days = ["Monday", "Tuesday", "Wednesday"];
+    // console.log(days.length);
+    // var newLength = days.push("Thursday");
+    // console.log(newLength);
+
+    // console.log("The max index of this array: " + days.indexOf("Thursday"));
+
+    // var removedElement = days.pop();
+    //
+    // console.log(days);
+    // console.log("The removed element was: " + removedElement);
+
+
+    // TODO: MINI-EXERCISE
+    //  -> var days = ["Monday", "Tuesday", "Wednesday"];
+    //  -> Create an array with the remaining days of the week
+    //  -> Call .forEach() on that NEW array
+    //  -> Inside the inline function you pass into .forEach():
+    //      -> add the remainingDay of the week to the old array
+    //  -> When the loop completes, console.log() the completed day of the week array
+
+    // var days = ["Monday", "Tuesday", "Wednesday"];
+    // var days2 = ["Thursday", "Friday", "Saturday", "Sunday"];
+    //
+    // days2.forEach(function (day){
+    //     days.push(day);
+    // });
+    //
+    // console.log(days);
+
+
+
+
+
+
+})()
