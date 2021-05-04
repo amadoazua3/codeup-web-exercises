29668a46cabff331442c6bcd2adb609378e59874
Amado Azua III <amado.azua3@gmail.com>
2021-05-04 09:10:46 -0500

    Javascript cube morning exercise

---
+++ js/javascript-cube-morning-exercise.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function cube(n){
+
+    return n * n * n;
+
+}
+
+console.log(cube(3));
+
+function cubeRoot(n){
+
+    return Math.cbrt(n).toFixed(4);
+
+}
+
+console.log(cubeRoot(3));
