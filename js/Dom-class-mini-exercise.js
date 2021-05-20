6ae63be3d6fecb4646ce848e235651201409ee62
Amado Azua III <amado.azua3@gmail.com>
2021-05-20 12:06:09 -0500

    DOM class mini exercise

--- js/Dom-class-mini-exercise.js
+++ js/Dom-class-mini-exercise.js
@@ -1,19 +1,24 @@
+
+// <!--// TODO: print the following items to the console: 1) A node that-->
+// <!--//  represents a div/item with a red border. 2) an array of elements-->
+// <!--//  whose text is dark-green. 3) an array of all paragraph elements-->
+
 // 1.
 let redBrd = document.getElementById("red-border");
-console.log(redBrd);
+// console.log(redBrd);
 
 //2. print an arr of elements whose text is dark green
 let drkGrn = document.getElementsByClassName("drk-grn")
-console.log(drkGrn);
+// console.log(drkGrn);
 
 // 3. print an array of ALL paragraphs
 let allPars = document.getElementsByTagName("p");
-console.log(allPars);
+// console.log(allPars);
 
 // STYLE ELEMENTS
 
 let yellowDiv = document.getElementById("yellow-border");
-console.log(yellowDiv);
+// console.log(yellowDiv);
 
 yellowDiv.style.border = "6px solid #f8f8f8";
 yellowDiv.style.color = "blue";
@@ -22,12 +27,5 @@ console.log(yellowDiv.childNodes[0]);
 
 // // change inner contents
 let yellowPar = document.getElementById("change-txt");
-console.log(yellowPar.innerText);
+// console.log(yellowPar.innerText);
 yellowPar.innerText = "hello world";
-
-
-// // console.log(document.getElementsByClassName("innerHTML")[0].innerHTML);
-// //
-// //
-// // let newEl = document.createElement((p))
-// document.getElementsByClassName("innerHTML")[0].innerHTML = "new text"
