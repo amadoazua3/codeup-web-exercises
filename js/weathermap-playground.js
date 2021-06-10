368b5393ecdde2068dd15ea00cab900d65cf1ca4
Amado Azua III <amado.azua3@gmail.com>
2021-06-10 16:51:09 -0500

    Weathermap api class lecture

--- js/weathermap-playground.js
+++ js/weathermap-playground.js
@@ -1,6 +1,21 @@
 
+// $.ajax({
+//     url: "http://api.openweathermap.org/data/2.5/weather",
+//     type: "GET",
+//     data: {
+//         APPID: OPEN_WEATHERMAP_TOKEN,
+//         // q: "Arlington, TX, US",
+//         lat: 32.7357,
+//         lon: -97.1081
+//     },
+//     success: function (data){
+//
+//         console.log(data);
+//     }
+// });
+
 $.ajax({
-    url: "http://api.openweathermap.org/data/2.5/weather",
+    url: "http://api.openweathermap.org/data/2.5/forecast",
     type: "GET",
     data: {
         APPID: OPEN_WEATHERMAP_TOKEN,
@@ -9,6 +24,7 @@ $.ajax({
         lon: -97.1081
     },
     success: function (data){
+
         console.log(data);
     }
 });
