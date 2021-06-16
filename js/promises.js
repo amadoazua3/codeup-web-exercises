7956a03c2dfefda883653da119f80141db67157d
Amado Azua III <amado.azua3@gmail.com>
2021-06-16 11:06:50 -0500

    Promise mini class exercise

--- js/promises.js
+++ js/promises.js
@@ -13,7 +13,26 @@ fetch('https://dog.ceo/api/breeds/image/random')
     })
     .catch(error => console.error(error));
 
-
-
-
-
+// create a second GET request to return the blog post with an id of 11 and display that blog post on the dom.
+fetch('https://jsonplaceholder.typicode.com/posts') // make a request - GET
+    .then(response => response.json())
+    .then(data =>{
+        // console.log(data); //
+        // console.log(data[10].id); // 11
+        return data[10].id; //11
+    })
+    .then(dataId => {
+        fetch(`https://jsonplaceholder.typicode.com/comments?postId=${dataId}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                data.forEach(comment => {
+                    console.log(comment);
+                    $("#comments").append(`<h4>${comment.name}</h4><hr><p>${comment.body}</p>`);
+                });
+            });
+    })
+    .catch(error => {
+        alert(error);
+        console.error(error);
+    });
